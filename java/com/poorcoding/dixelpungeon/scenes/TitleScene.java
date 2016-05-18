7c6dc8b9231552d49ef058f15667896d66e9b54e
Arfonzo J. Coward <arfonzo@gmail.com>
2016-05-18 11:19:11 +0100

    Refactor version_dixel from PD Classes code into Dixel codebase. - Temporary placement, to be refactored laterwards.

--- java/com/poorcoding/dixelpungeon/scenes/TitleScene.java
+++ java/com/poorcoding/dixelpungeon/scenes/TitleScene.java
@@ -42,7 +42,9 @@ public class TitleScene extends PixelScene {
 	private static final String TXT_HIGHSCORES	= "Rankings";
 	private static final String TXT_BADGES		= "Badges";
 	private static final String TXT_ABOUT		= "About";
-	
+
+	public static String version_dixel = "43";
+
 	@Override
 	public void create() {
 		
@@ -135,7 +137,7 @@ public class TitleScene extends PixelScene {
 			btnHighscores.setPos( w / 2, btnPlay.top() );
 		}
 		
-		BitmapText version = new BitmapText( "Dix r" + Game.version_dixel + " (based on PD v " + Game.version + ")", font1x );
+		BitmapText version = new BitmapText( "Dix r" + version_dixel + " (based on PD v " + Game.version + ")", font1x );
 		version.measure();
 		version.hardlight( 0x888888 );
 		version.x = w - version.width();
