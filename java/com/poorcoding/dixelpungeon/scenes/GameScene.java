afee7144eb436806acc6ff2ce243cdb25e470de1
Arfonzo J. Coward <arfonzo@gmail.com>
2016-05-18 09:44:42 +0100

    Update GameScene Welcome/Welcome Back strings.

--- java/com/poorcoding/dixelpungeon/scenes/GameScene.java
+++ java/com/poorcoding/dixelpungeon/scenes/GameScene.java
@@ -77,8 +77,8 @@ import com.poorcoding.utils.Random;
 
 public class GameScene extends PixelScene {
 	
-	private static final String TXT_WELCOME			= "Welcome to the level %d of Pixel Dungeon!";
-	private static final String TXT_WELCOME_BACK	= "Welcome back to the level %d of Pixel Dungeon!";
+	private static final String TXT_WELCOME			= "Welcome to the level %d of Dixel Pungeon!";
+	private static final String TXT_WELCOME_BACK	= "Welcome back to the level %d of Dixel Pungeon!";
 	private static final String TXT_NIGHT_MODE		= "Be cautious, since the dungeon is even more dangerous at night!";
 	
 	private static final String TXT_CHASM	= "Your steps echo across the dungeon.";
