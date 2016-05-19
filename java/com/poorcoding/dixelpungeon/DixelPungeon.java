5c15747fc9afb1e6a9aafbf8e5a27bb6e51f6a3d
Arfonzo J. Coward <arfonzo@gmail.com>
2016-05-19 08:49:36 +0100

    Version bump.

--- java/com/poorcoding/dixelpungeon/DixelPungeon.java
+++ java/com/poorcoding/dixelpungeon/DixelPungeon.java
@@ -35,7 +35,7 @@ import com.poorcoding.dixelpungeon.scenes.TitleScene;
 
 public class DixelPungeon extends Game {
 
-	public static String version_dixel = "48";
+	public static String version_dixel = "50";
 	
 	public DixelPungeon() {
 		super( TitleScene.class );
