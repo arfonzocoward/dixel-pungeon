d09c61c698be768d8c43ec71edb38db6eb70ba60
Arfonzo J. Coward <arfonzo@gmail.com>
2016-05-21 17:40:49 +0100

    Update signs, and reshuffle the order of some.

--- java/com/poorcoding/dixelpungeon/levels/features/Sign.java
+++ java/com/poorcoding/dixelpungeon/levels/features/Sign.java
@@ -34,17 +34,16 @@ public class Sign {
 		"What are you doing here?!";
 	
 	private static final String[] TIPS = {
-		"Don't overestimate your strength, use weapons and armor you can handle.\n\nRest at the _campfires_. Access the _Soulforge_ there from the game menu.",
-		"Not all doors in the dungeon are visible at first sight. If you are stuck, _search_ for hidden doors.",
-		"Remember, that raising your strength is not the only way to access better equipment. You can go " +
-			"the other way, _lowering_ its strength requirement with Scrolls of Upgrade. You can also spend souls to upgrade at _Soulforges_.",
-		"You can spend your gold in _shops_ on deeper levels of the dungeon. The first one is on the 6th level.",
-			
+		"Don't overestimate your strength, use weapons and armor you can handle.\n\nRest at campfires. Access the Soulforges to upgrade.",
+		"You can spend your gold in shops, or sell off inventory.",
+		"Identify your potions and scrolls as soon as possible. Don't put it off to the moment when you actually need them.",
+		"Not all doors in the dungeon are visible at first sight. If you are stuck, search for hidden doors.",
+
 		"Beware of Goo!",
 		
 		"Pixel-Mart - all you need for successful adventure!",
-		"Identify your potions and scrolls as soon as possible. Don't put it off to the moment " +
-			"when you actually need them.",
+		"Remember, that raising your strength is not the only way to access better equipment. You can go " +
+				"the other way, lowering its strength requirement with Scrolls of Upgrade.\n\nYou can also spend souls to upgrade at Soulforges.",
 		"Being hungry doesn't hurt, but starving does hurt.",
 		"Surprise attack has a better chance to hit. For example, you can ambush your enemy behind " +
 			"a closed door when you know it is approaching.",
